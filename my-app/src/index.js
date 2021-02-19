da476772a0f2b73835d0ba5181165fe8f347a635
etharinoh <etharinohallberry@gmail.com>
2021-02-19 17:37:54 +0000

    React Start

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,10 +4,75 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+
+const user = {
+  firstName: 'ethan',
+  lastName: 'hall'
+}
+
+const fullname = 'Ethan Hall';
+
+const divelement = <div tabIndex="0"></div>
+const imgelement = <img src={user.avatarURl}></img>
+const element = (
+  <div>
+    {getGreeting()}
+    {divelement}
+    {imgelement}
+  </div>
+);
+
+//functions
+function getGreeting(user){
+  if(user){
+    return <h1>Hello, {formatName(user)}</h1>;
+  }
+  else{
+    return <h1> Hello, Stranger</h1>;
+  }
+}
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+
+
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+    //Lifecycle methods
+  componentDidMount() { 
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+   }
+  componentWillUnmount() { 
+    clearInterval(this.timerID);
+   }
+
+   tick(){ 
+     this.setState({
+      date: new Date()
+     });
+   }
+  render() {
+    return (
+    <div>
+      <h1>Hello world</h1>
+      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+    </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Clock />,
   document.getElementById('root')
 );
 
